16494585bcfcb6ef450fcbae62674a5158a2fe13
Ketchupbombe <LukasHonold1407@gmail.com>
2017-07-09 10:30:34 +0200

    + main (onEnable)

---
+++ src/main/java/de/ketchupbombe/HelloWorldMaven.java
@@ -0,0 +1,14 @@
+package de.ketchupbombe;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by Hp on 09.07.2017.
+ */
+public class HelloWorldMaven extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+}
